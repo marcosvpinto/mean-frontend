555ef0ab523f619de366cd20432a1916da8cb25b
mvpdev@gmail.com <v93i39u8>
2018-07-10 10:41:37 -0300

    ligando o frontend com o backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MatToolbarModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule,
+         MatFormFieldModule,
+         MatInputModule,
+         MatOptionModule,
+         MatSelectModule,
+         MatIconModule,
+         MatButtonModule,
+         MatCardModule,
+         MatTableModule,
+         MatDividerModule,
+         MatSnackBarModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+
+import { IssueService } from './issue.service';
 
 const routes: Routes = [
   {path: 'create', component: CreateComponent},
@@ -24,10 +39,23 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes),
-    MatToolbarModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [IssueService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { IssueService } from '../../issue.service';
+
 import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
+
+import { Issue } from '../../issue.model';
+import { IssueService } from '../../issue.service';
 
 @Component({
   selector: 'app-list',
@@ -9,9 +13,33 @@ import { Router } from '@angular/router';
 })
 export class ListComponent implements OnInit {
 
+  issues: Issue[];
+  displayedColumns = ['title', 'responsible', 'severity', 'status', 'actions'];
+
   constructor(private issueService: IssueService, private router: Router) { }
 
   ngOnInit() {
+    this.fetchIssues();
+  }
+
+  fetchIssues() {
+    this.issueService
+      .getIssues()
+      .subscribe((data: Issue[]) => {
+        this.issues = data;
+        console.log('Data requested... ');
+        console.log(this.issues);
+      });
+  }
+
+  editIssue(id) {
+    this.router.navigate([`/edit/${id}`]);
+  }
+
+  deleteIssue(id) {
+    this.issueService.deleteIssue(id).subscribe(() => {
+      this.fetchIssues();
+    });
   }
 
 }
